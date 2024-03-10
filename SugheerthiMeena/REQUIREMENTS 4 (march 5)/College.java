d5baf8d84f304c6c1c5424f8d99b1e00cb8d0461
SugheerthiMeenaSK <161918739+SugheerthiMeenaSK@users.noreply.github.com>
2024-03-10 15:40:45 +0530

    Add files via upload

---
+++ SugheerthiMeena/REQUIREMENTS 4 (march 5)/College.java
@@ -0,0 +1,24 @@
+package edu.steria.training;
+import lombok.*;
+import java.util.Date;
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class College {
+    private String name;
+    private String website;
+    private String mobile;
+    private String founder;
+    private Integer numberOfDept;
+    private String location;
+    private Date startingDate;
+
+    static College createCollege(String name, String website, String mobile, String founder, Integer numberOfDept, String location, Date startingDate)
+    {
+
+        return new College(name, website, mobile, founder, numberOfDept, location, startingDate);
+    }
+
+}

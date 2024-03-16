e35b22d41dbde42a5e368c6d799151df70f8c2ca
Sayyad0907 <161918893+Sayyad0907@users.noreply.github.com>
2024-03-16 09:58:10 +0530

    Update addcart.java

--- Abdul Sayyad/Ecommerce3/src/main/java/edu/training/addcart.java
+++ Abdul Sayyad/Ecommerce3/src/main/java/edu/training/addcart.java
@@ -37,28 +37,36 @@ public class addcart extends HttpServlet {
 		String uemail = ActionServlet.customerEmail;
 		int pid =Integer.parseInt(request.getParameter("pid"));
 		int pquant = 1;
+		response.getWriter().println(pid);
 		try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root","root");
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("select quantity from product where pid = "+pid);
+        	RequestDispatcher rd = request.getRequestDispatcher("/viewCart");
+
             if(rs.next() && pquant <= rs.getInt(1)) {
                 ResultSet rs1 = statement.executeQuery("select quantity from cart where pid = "+pid+" and cusemail = '"+ActionServlet.customerEmail+"'");
                 int initquant = 0;
                 if(rs1.next()) {
-                	initquant  = rs1.getInt(1);
-                }
-                statement.executeUpdate("delete from cart where pid = "+ pid+" and cusemail = '"+ActionServlet.customerEmail+"'");
-            	RequestDispatcher rd = request.getRequestDispatcher("/viewCart");
-            	PreparedStatement ps = connection.prepareStatement("insert into cart values(?,?,?)");
-            	ps.setString(1,uemail);
-            	ps.setInt(2,pid );
-            	ps.setInt(3,pquant + initquant);
+                initquant  = rs1.getInt(1);
+    
+            	PreparedStatement ps = connection.prepareStatement("update cart set quantity = quantity + ? where cusemail = ? and pid = ?");
+            	ps.setInt(1,1);
+            	ps.setString(2,uemail);
+            	ps.setInt(3,pid);
             	ps.executeUpdate();
-                rd.forward(request, response);
+                }else {
+                	PreparedStatement ps = connection.prepareStatement("insert into cart values(?,?,?)");
+                	ps.setString(1,uemail);
+                	ps.setInt(2,pid );
+                	ps.setInt(3,1);
+                	ps.executeUpdate();
+                }
+               rd.forward(request, response);
+
             }
             else {
-            	RequestDispatcher rd = request.getRequestDispatcher("addcart.jsp");
             	request.setAttribute("msg", "<h2 style = 'color:red'>Product or quantity Not Available<h2>");
                 rd.forward(request, response);
             }

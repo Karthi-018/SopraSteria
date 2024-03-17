69414198f89f7a2355c8e100ec2cc1ae982a5044
GuduriHarsha <161919638+GuduriHarsha@users.noreply.github.com>
2024-03-18 00:10:51 +0530

    Add files via upload

---
+++ Harsha/Ecommerce with DB/Dbconnection.java
@@ -0,0 +1,25 @@
+package org.edu;
+import java.sql.*;
+public class Dbconnection
+{
+    public static Connection getConnection()
+    {
+        Connection con=null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Demo", "root", "root");
+            System.out.println("connected...");
+
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println(e);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+        return con;
+
+    }
+}

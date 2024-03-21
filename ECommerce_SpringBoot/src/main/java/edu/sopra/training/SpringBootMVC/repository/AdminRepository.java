855ffabf8410842e2c45aa87bfb4dfab6f172438
Shanmitha-12 <161918920+Shanmitha-12@users.noreply.github.com>
2024-03-21 14:39:21 +0530

    Delete ECommerce_SpringBoot directory

--- ECommerce_SpringBoot/src/main/java/edu/sopra/training/SpringBootMVC/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sopra.training.SpringBootMVC.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import edu.sopra.training.SpringBootMVC.model.AdminLogin;
-
-public interface AdminRepository extends CrudRepository<AdminLogin, Integer> {
-
-}
